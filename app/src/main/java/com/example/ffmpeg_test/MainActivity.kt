907ec6343a52be3cea682828bb058d1db0661444
yk <yk@ykdeMacBook-Pro.local>
2023-10-09 23:36:19 +0800

    Initial commit

---
+++ app/src/main/java/com/example/ffmpeg_test/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.ffmpeg_test
+
+import android.annotation.SuppressLint
+import android.graphics.PixelFormat
+import android.os.Bundle
+import android.os.Environment
+import android.util.Log
+import android.view.SurfaceHolder
+import androidx.appcompat.app.AppCompatActivity
+import com.example.ffmpeg_test.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(),  SurfaceHolder.Callback{
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initView()
+    }
+
+    private fun initView() {
+        binding.mainTvTest.text = ffmpegInfo()
+        Log.d(TAG, "onCreate: ${getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS)}")
+
+        binding.mainSvTest.holder.addCallback(this)
+        binding.mainTvTest.setOnClickListener {
+            Log.d(TAG, "onCreate: setOnClickListener")
+            //子线程进行视频渲染
+            // Thread { nativePlayVideo(path, binding.mainSvTest.holder.surface) }.start()
+            nativePlayVideo(path, binding.mainSvTest.holder.surface)
+        }
+        binding.mainSvTest.holder.setFormat(PixelFormat.RGBA_8888)
+    }
+
+    override fun surfaceCreated(holder: SurfaceHolder) {
+        Log.d(TAG, "surfaceCreated: ")
+    }
+
+    override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
+    }
+
+    override fun surfaceDestroyed(holder: SurfaceHolder) {
+    }
+
+    companion object {
+        private const val TAG = "MainActivity11"
+        @SuppressLint("SdCardPath")
+        private val path = "/sdcard/DCIM/Camera/VID_20231008_094739.mp4"
+        init {
+            System.loadLibrary("native-lib")
+        }
+    }
+}
---
+++ app/src/main/java/com/example/ffmpeg_test/NativeMethod.kt
@@ -0,0 +1,7 @@
+package com.example.ffmpeg_test
+
+import android.view.Surface
+
+external fun ffmpegInfo(): String
+
+ external fun nativePlayVideo(path: String, surface: Surface)
