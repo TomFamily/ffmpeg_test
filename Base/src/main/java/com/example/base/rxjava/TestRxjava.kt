3a54966262e982fad6616ed4221bdf6dedb91680
yk <1848304407@qq.com>
2023-12-14 00:38:41 +0800

    [FEATURE]: [添加RxJava测试代码 - 2]    - 分析:[无]    - 风险:[无]    - 解决方案:[无]    - 测试建议:[无]    - 适用范围:[无]    - 测试机型:[无]    - 自测结果:[华为ANE-AL00 自测正常]

--- Base/src/main/java/com/example/base/rxjava/TestRxjava.kt
+++ Base/src/main/java/com/example/base/rxjava/TestRxjava.kt
@@ -2,6 +2,7 @@ package com.example.base.rxjava
 
 import android.util.Log
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Completable
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.core.ObservableTransformer
 import io.reactivex.rxjava3.core.Single
@@ -16,7 +17,7 @@ import java.util.concurrent.TimeUnit
 
 private const val TAG = "TestRxjava"
 
-fun dealRealTimeEvent() {
+fun testWindow() {
     /**
      * 通过window创建一个一秒时长的窗口，取窗口中的3个数据（为了确保至少有三个数据），当满足条件数据，往下执行
      *
@@ -27,7 +28,6 @@ fun dealRealTimeEvent() {
      * 但是它不是一个 List 类型，因此尝试直接将其转换为 List 会导致 ClassCastException。
      *
      * 缺点：会启动一个定时器
-     *
      */
     Observable.range(10,20)
         .observeOn(AndroidSchedulers.mainThread())
@@ -39,6 +39,9 @@ fun dealRealTimeEvent() {
             Log.d(TAG, "testKJ222: $it")
         }.dispose()
 
+}
+
+fun testBuffer() {
     /**
      * 每5个数据归位一组，一组数据的第一个与最后一个的时间间隔在1秒内，就往下执行
      */
@@ -53,18 +56,24 @@ fun dealRealTimeEvent() {
         }.dispose()
 }
 
-fun testThread() {
+fun testConcatWith() {
+    /**
+     * concatWith 操作符用于将当前Observable的发射物品与另一个Observable的发射物品连接起来，形成一个新的Observable。
+     * 它会按照顺序先发射当前Observable的所有数据，然后再发射另一个Observable的数据。
+     */
     Single.just(0)
         .cache()
         .concatWith {  }
         .subscribe {
-            Log.d(TAG, "testThread: $it") 
+            Log.d(TAG, "testThread: $it")
         }.dispose()
+}
 
-    /**
-     * 1、subscribeOn 必须写在 observeOn 之前才会有效
-     * 2、多个 subscribeOn， 只有第一个 subscribeOn 会生效
-     */
+/**
+ * 1、subscribeOn 必须写在 observeOn 之前才会有效
+ * 2、多个 subscribeOn， 只有第一个 subscribeOn 会生效
+ */
+fun testThread() {
     Observable.create {
         it.onNext(false)
     }
@@ -79,9 +88,9 @@ fun testThread() {
         .observeOn(AndroidSchedulers.mainThread())
         .doOnNext { Log.d(TAG, "testThread4: ${Thread.currentThread()}") }
         .subscribe()
+}
 
-
-    Log.d(TAG, "testThread: 222 ${System.currentTimeMillis()}")
+fun testEmpty() {
     /**
      * empty() ： 直接发送 onComplete() 事件
      * Observable.empty 不会发送值，所以：doOnNext、subscribe 操作符都是无法监听到数据的，
@@ -100,7 +109,9 @@ fun testThread() {
         .subscribe {
             Log.d(TAG, "testThread: 222")
         }.dispose()
+}
 
+fun testFlatMap() {
     Observable.create {
         it.onNext(true)
     }
@@ -127,10 +138,10 @@ fun testThread() {
         .observeOn(AndroidSchedulers.mainThread())
         .doOnNext {
             Log.d(TAG, "testThread: state4 ${System.currentTimeMillis()}")
-        }
-        .subscribe().dispose()
+        }.subscribe().dispose()
+}
 
-
+fun testCompose() {
     /**
      * 在RxJava中，compose操作符用于将一系列的操作符封装为一个可重用的操作符。它可以帮助简化代码，并提高代码的可读性和可维护性。
      * 使用compose操作符时，可以通过：ObservableTransformer 创建一个自定义的操作符，该操作符由多个其他操作符组成。
@@ -155,8 +166,9 @@ fun testThread() {
         .subscribe {
 
         }.dispose()
+}
 
-
+fun testDefer() {
     /**
      * defer：直到被观察者被订阅后才会创建被观察者
      *
@@ -179,9 +191,124 @@ fun testThread() {
     observable.subscribe {
         Log.d("Example", "Subscriber 2 received: $it")
     }.dispose()
+}
+
+fun testAndThen() {
+    /**
+     * andThen 可以组合两个 Completable，在前一个 Completable 执行完成后，才会再执行第二个 Completable
+     * 在本例中，将 completable1、completable2 组合，在 completable1 执行完后，completable2才会执行
+     */
+    val completable1 = Completable.fromRunnable { Log.d(TAG, "testAndThen: completable1") }
+    val completable2 = Completable.fromRunnable { Log.d(TAG, "testAndThen: completable2") }
+
+    completable1.andThen(completable2)
+        .subscribe {
+            Log.d(TAG, "testAndThen: All completable done")
+        }.dispose()
+}
+
+fun testZip() {
+    /**
+     * zip用于将多个 Observable 发射的数据按照一个函数组合成一个新的数据项并发射出去。这个操作符能够将多个
+     * Observables 的数据项进行配对，从而创建一个新的数据项集合。
+     *
+     * 注意：
+     *   1、zip 操作符会等待所有 Observable 都至少发射了一个数据项后再开始组合。如果某个 Observable 没有发射数据，
+     *      zip 将不会发射任何数据项，直到所有 Observables 都发射了至少一项数据。
+     *   2、如果其中一个 Observable 发射的数据项比另一个 Observable 发射的数据项更少，那么 zip 操作符只会发射与
+     *      最少项数量相同的数据项。多出来的数据项将被忽略。
+     */
+    val numbers = Observable.just(1, 2, 3, 4, 5)
+    val words = Observable.just("One", "Two", "Three", "Four", "Five")
+
+    Observable.zip(numbers, words) { number, word ->
+        "$number - $word"
+    }.subscribe { result ->
+        Log.d(TAG, "testZip: $result")
+        }.dispose()
+}
+
+fun testZipWith() {
+    /**
+     * zipWith 是一个操作符，用于将多个 Observable 的发射物按照指定的函数结合在一起，产生一个新的值。
+     * zipWith 操作符可以用于将两个或多个 Observable 发射的数据项组合在一起，并且可以定义一个函数来处理这些组合。
+     *
+     * 注意：如果两个 Observable 的发射物数量不同，zipWith 会以最短的 Observable 为准。也就是说，
+     *      如果其中一个 Observable 发射完毕，zipWith 将停止发射新的数据项。
+     *
+     * 在本例中，定义了两个range，都发送 int 值，在 zipWith 中将两个数据组合为一个 Pair
+     *
+     * skip：跳过前 N 个数据
+     */
+    Observable.range(1, 6)
+        .zipWith(Observable.range(1, Int.MAX_VALUE)) { t1, t2 ->
+            t1 to t2
+        }
+        .concatMap { Observable.just(it).delay(1, TimeUnit.SECONDS) }
+        .skip(2)
+        .doOnComplete { Log.d(TAG, "testZipWith: doOnComplete ") }
+        .subscribe {
+            Log.d(TAG, "testZipWith: subscribe $it")
+        }.dispose()
+}
+
+fun testConcatMap() {
+    /**
+     * 用于将一个 Observable 的每个发射物映射成一个 Observable，并将这些 Observables 按照顺序连接起来。
+     * 这操作符的主要目的是保证发射的顺序，确保每个 Observable 发射的数据项按照原始的顺序被订阅者接收。
+     *
+     * 注意：
+     *  1、concatMap 与 flatMap 类似，但是 concatMap 保证了发射的顺序，而 flatMap 则可以交错发射。
+     *  2、对于 concatMap 中的每个映射后的 Observable，如果有一个 Observable 抛出错误，则整个序列会立即终止，
+     *      并且错误会传递给订阅者。
+     */
+    Observable.just(1, 2, 3, 4, 5)
+        .concatMap {
+            Observable.just(it).delay(100, TimeUnit.MILLISECONDS) // 模拟耗时操作
+        }
+        .subscribe()
+        .dispose()
+}
+
+fun testCombineLatest() {
+    /**
+     * combineLatest可以组合多个数据源，多个 Observables 中的任何一个发射数据时，结合最近发射的数据项，
+     * 通过一个函数组合它们并发射出去。多用于数据联动的场景
+     *
+     * 参数：
+     *   combineLatest 接收多个 Observables 作为参数，以及一个函数 combiner 用于组合这些 Observables 最近发射的数据。
+     *   combiner 函数接收每个 Observable 最近发射的数据项，并根据给定的逻辑生成一个新的数据项。（监听数据应该在： subscribe）
+     *
+     * 注意：
+     *  1、combineLatest 操作符会在任何一个 Observable 发射数据时，结合所有 Observables 最近发射的数据项。因此，
+     *     如果其中一个 Observable 发射了多个数据项而另一个没有发射新的数据，combineLatest 会使用最新的那个数据项来组合。
+     *  2、如果有一个 Observable 没有发射任何数据，combineLatest 将不会发射任何数据项。
+     */
+    val numbers = Observable.just(1, 2, 3, 4)
+    val words = Observable.just("One", "Two", "Three", "Four", "Five")
+
+    Observable.combineLatest(numbers, words) { number, word -> "$number - $word" }
+        .subscribe { result ->
+            Log.d(TAG, "testCombineLatest: $result")
+        }.dispose()
+}
+
+fun testAmbArray() {
+    /**
+     * ambArray 用于从 多个 Observable 中选择第一个发射数据的 Observable，并只订阅这个 Observable。
+     * "amb" 是 "ambitious"（有野心的）的缩写，表示它会选择第一个发射数据的 Observable，并且忽略其他的 Observables。
+     *
+     * 注意：
+     *  1、ambArray 会选择第一个发射数据的 Observable，并且只会订阅这个 Observable，忽略其他的 Observables。
+     *  2、如果多个 Observables 中有一个或多个同时发射第一个数据，ambArray 会选择其中之一，而忽略其他的。
+     */
+    val observable1 = Observable.just(1, 2, 3).delay(1, TimeUnit.SECONDS)
+    val observable2 = Observable.just(4, 5, 6)
 
     /**
-     * concatWith操作符用于将当前Observable的发射物品与另一个Observable的发射物品连接起来，形成一个新的Observable。它会按照顺序先发射当前Observable的所有数据，然后再发射另一个Observable的数据。
+     * 此例中，只会监听 observable2 的数据
      */
-
+    Observable.ambArray(observable1, observable2).subscribe { result: Int? ->
+            Log.d(TAG, "testAmbArray: $result")
+        }.dispose()
 }
